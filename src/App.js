9bdf2d3632edc6eed302315e89e86ddbbe40e283
Yanina Torres <yaninatc14@gmail.com>
2023-08-24 21:29:54 -0600

    proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [pokemonNames, setPokemonNames] = useState([]);
+
+  const fetchPokemonNames = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+      .then(response => {
+        const pokemonData = response.data.results;
+        const names = pokemonData.map(pokemon => pokemon.name);
+        setPokemonNames(names);
+      })
+      .catch(err=>res.json({message: "algo salio mal", error: err}));   
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fetch Pokemon</h1>
+        <button onClick={fetchPokemonNames}>Fetch Pokemon</button>
+        <div className="pokemon-list">
+          <h2>Pokemon Names:</h2>
+          <ul>
+            {pokemonNames.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
